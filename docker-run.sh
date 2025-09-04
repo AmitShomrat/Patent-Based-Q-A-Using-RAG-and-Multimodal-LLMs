e4fdba6dd74e1f398fa2c051772a76ae3527066c
Amit Shomrat <Amit56sh@gmail.com>
2025-09-04 13:15:38 +0300

    docker-run.sh update sending CMD argument will activate the container with the CMD otherwise activation with interactive terminal

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run --rm --gpus all patent-rag-cuda:v1.0
+if [ $1 == "CMD" ]; then
+    docker run --rm --gpus all patent-rag-cuda:v1.1
+else
+    docker run -it --rm --gpus all --entrypoint /bin/bash patent-rag-cuda:v1.1
+fi
