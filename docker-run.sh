1d5727eec37519c6f85159339b6f0e1482e02e62
Amit Shomrat <Amit56sh@gmail.com>
2025-09-08 19:31:40 +0300

    docker-run.sh defined to be exposed to a shared volume

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $1 == "CMD" ]; then # CMD - run the CMD in the Dockerfile
-    docker run -p 8000:8000 --rm --gpus all patent-rag-cuda:v1.3
+    docker run -v $(pwd)/rag_inputs:/app/rag_inputs -p 8000:8000 --rm --gpus all patent-rag-cuda:v1.3
 else # default - run the container in interactive mode
-    docker run -p 8000:8000 -it --rm --gpus all --entrypoint /bin/bash patent-rag-cuda:v1.3
+    docker run -v $(pwd)/rag_inputs:/app/rag_inputs -p 8000:8000 -it --rm --gpus all --entrypoint /bin/bash patent-rag-cuda:v1.3
 fi
